91ee4916dc5d735886db3d34cabfbaa32f07d27f
Romain� <=>
2023-11-30 13:39:01 +0100

    1. Afficher le premier tweet

--- tweets/index.js
+++ tweets/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const tweets = [
   {
     id: 1,
@@ -30,3 +31,42 @@ const tweets = [
     likes: 20
   }
 ]
+
+
+/**
+ * @param {Object} informations
+ * @param {string} informations.content
+ * @param {string} informations.author
+ * @param {number} informations.likes
+ * @returns {HTMLLIElement}
+ * @example
+ * createTweet({
+ *  content: 'Lorem ipsum',
+ *  author: 'Ada',
+ *  likes: 10
+ * })
+ */
+function createTweet(informations) {
+  const tweet = document.createElement('li')
+  tweet.classList.add('tweet')
+  Object.entries(informations).forEach(([className, elementContent]) => {
+    if (className === 'id') return
+    const element = document.createElement('p')
+    element.classList.add(className)
+    element.textContent = elementContent
+    tweet.appendChild(element)
+  })
+  return tweet
+}
+
+
+const tweetsListElement = document.querySelector('#tweets-list')
+tweetsListElement.append(createTweet(tweets[0]))
+
+// J'ai crée une fonction `createTweet` qui permet de créer un tweet plus facilement en lui passant les bons paramètres.
+// Elle prend en paramètre un objet avec les informations du tweet.
+// La fonction crée un élément `li` avec la classe `tweet`.
+// Ensuite, elle parcourt les clés et valeurs de l'objet passé en paramètre.
+// Pour chaque clé/valeur, elle crée un élément `p` avec la bonne classe et le bon contenu.
+// Enfin, elle ajoute l'élément `p` à l'élément `li`.
+// Et elle retourne l'élément `li`.
